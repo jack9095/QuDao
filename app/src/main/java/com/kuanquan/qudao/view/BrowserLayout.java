3e5f50e0cbc54cc89c27768bd0b5f83531cb6600
wangfei <gaodun123com>
2018-06-06 11:27:47 +0800

    底部布局

--- app/src/main/java/com/kuanquan/qudao/view/BrowserLayout.java
+++ app/src/main/java/com/kuanquan/qudao/view/BrowserLayout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
