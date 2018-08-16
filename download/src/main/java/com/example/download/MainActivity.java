74c91b3fca75868503cdc22a6479696104786bda
wangfei <gaodun123com>
2018-08-16 13:21:44 +0800

    网络请求的封装

---
+++ download/src/main/java/com/example/download/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.download;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 第三方框架
+ * https://github.com/lingochamp/FileDownloader/blob/master/README-zh.md
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
