758097b681eb8d7b30bd88d43aece6b3df1e448e
wangfei <gaodun123com>
2018-08-29 17:02:54 +0800

    jetpack

---
+++ jetpack/src/main/java/com/example/jetpack/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jetpack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * https://blog.csdn.net/wangcheng_/article/details/79385417
+ * ViewModel+LiveData 使用
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ jetpack/src/main/java/com/example/jetpack/viewModel/UserProfileViewModel.java
@@ -0,0 +1,14 @@
+package com.example.jetpack.viewModel;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by fei.wang on 2018/8/29.
+ *
+ */
+
+public class UserProfileViewModel extends ViewModel {
+
+
+
+}
